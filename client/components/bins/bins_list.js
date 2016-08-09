3680d928ee20f36b57f7d9f605bb8812616ec54b
rayGreycloud <rayGreycloud@gmail.com>
2016-08-08 17:03:17 -0700

    Add remove bin button and meteor method

--- client/components/bins/bins_list.js
+++ client/components/bins/bins_list.js
@@ -3,14 +3,23 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/collections/bins';
 
 class BinsList extends Component {
+  onBinRemove(bin) {
+    Meteor.call('bins.remove', bin);
+  }
+
   renderList() {
     return this.props.bins.map(bin => {
       return (
         <li className="list-group-item" key={bin._id}>
           Bin {bin._id}
+          <span className="pull-right">
+            <button
+              className="btn btn-danger"
+              onClick={() => this.onBinRemove(bin)}>Remove</button>
+          </span>
         </li>
       );
-    })
+    });
   }
 
   render() {
