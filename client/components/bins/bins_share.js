5b6df6e80708b71a7fbce832606d49bf2b63b308
rayGreycloud <rayGreycloud@gmail.com>
2016-08-10 12:48:43 -0700

    Add input field and button to BinsShare

--- client/components/bins/bins_share.js
+++ client/components/bins/bins_share.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class BinsShare extends Component {
+  onShareClick() {
+    const email = this.refs.email.value;
+    
+  }
+
   render() {
     return (
       <div>
-        <footer className="bins-share">BinsShare</footer>
+        <footer className="bins-share">
+          <div className="input-group">
+            <input ref="email" className="form-control" />
+            <div className="input-group-btn">
+              <button
+                onClick={this.onShareClick.bind(this)}
+                className="btn btn-default">
+                Share Bin
+              </button>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
