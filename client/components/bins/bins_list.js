e2ef845ae4529ab63eff479378d859a04868c400
rayGreycloud <rayGreycloud@gmail.com>
2016-08-10 13:57:10 -0700

    Add meteor subscription for BinsList and BinsMain

--- client/components/bins/bins_list.js
+++ client/components/bins/bins_list.js
@@ -35,6 +35,7 @@ class BinsList extends Component {
 
 export default createContainer(() => {
   Meteor.subscribe('bins');
+  Meteor.subscribe('sharedBins');
 
   return { bins: Bins.find({}).fetch() };
 }, BinsList);
